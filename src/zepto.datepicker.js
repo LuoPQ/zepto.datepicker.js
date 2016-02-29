d2b09bc18a329317d6fd8391bc1978d52aee7d19
LuoPQ <284973314@qq.com>
2016-02-29 16:38:08 +0800

    addAuthorInfo

--- src/zepto.datepicker.js
+++ src/zepto.datepicker.js
@@ -1,4 +1,10 @@
-﻿; (function ($, window, document, undefined) {
+﻿/**
+ * zepto.datepicker.js v0.1.0
+ * MIT License
+ * author info pls visit: http://luopq.com
+ * for more info pls visit: https://github.com/LuoPQ/zepto.datepicker.js
+ */
+; (function ($, window, document, undefined) {
 
     //#region Date扩展
 
